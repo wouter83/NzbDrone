5f65fcf194bc377ae39e893b61597ff382b7c410
Mark McDowall <markus.mcd5@gmail.com>
2014-08-03 00:26:55 -0700

    Initial work for media browser, library update does not work

---
+++ src/NzbDrone.Core/Notifications/MediaBrowser/MediaBrowser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation.Results;
+using NzbDrone.Common;
+using NzbDrone.Core.Tv;
+
+namespace NzbDrone.Core.Notifications.MediaBrowser
+{
+    public class MediaBrowser : NotificationBase<MediaBrowserSettings>
+    {
+        private readonly IMediaBrowserService _mediaBrowserService;
+
+        public MediaBrowser(IMediaBrowserService mediaBrowserService)
+        {
+            _mediaBrowserService = mediaBrowserService;
+        }
+
+        public override string Link
+        {
+            get { return "http://http://mediabrowser.tv/"; }
+        }
+
+        public override void OnGrab(String message)
+        {
+            const string title = "NzbDrone [TV] - Grabbed";
+
+            if (Settings.Notify)
+            {
+                _mediaBrowserService.Notify(Settings, title, message);
+            }
+        }
+
+        public override void OnDownload(DownloadMessage message)
+        {
+            const string title = "NzbDrone [TV] - Downloaded";
+
+            if (Settings.Notify)
+            {
+                _mediaBrowserService.Notify(Settings, title, message.Message);
+            }
+
+            if (Settings.UpdateLibrary)
+            {
+                _mediaBrowserService.Update(Settings, message.Series);
+            }
+        }
+
+        public override void AfterRename(Series series)
+        {
+            if (Settings.UpdateLibrary)
+            {
+                _mediaBrowserService.Update(Settings, series);
+            }
+        }
+
+        public override ValidationResult Test()
+        {
+            var failures = new List<ValidationFailure>();
+
+            failures.AddIfNotNull(_mediaBrowserService.Test(Settings));
+
+            return new ValidationResult(failures);
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Notifications/MediaBrowser/MediaBrowserProxy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NLog;
+using NzbDrone.Core.Rest;
+using RestSharp;
+
+namespace NzbDrone.Core.Notifications.MediaBrowser
+{
+    public class MediaBrowserProxy
+    {
+        private readonly Logger _logger;
+
+        public MediaBrowserProxy(Logger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Notify(MediaBrowserSettings settings, String title, String message)
+        {
+            var request = new RestRequest("Notifications/Admin");
+
+            request.RequestFormat = DataFormat.Json;
+
+            request.AddBody(new
+                            {
+                                Name = title,
+                                Description = message,
+                                ImageUrl = "https://raw.github.com/NzbDrone/NzbDrone/develop/Logo/64.png",
+                            });
+
+            ProcessRequest(request, settings);
+        }
+
+        public void Update(MediaBrowserSettings settings, Int32 tvdbId)
+        {
+            var request = new RestRequest("Library/Series/Updated");
+
+            request.AddParameter("tvdbid", tvdbId, ParameterType.QueryString);
+
+            ProcessRequest(request, settings);
+        }
+
+        private String ProcessRequest(IRestRequest request, MediaBrowserSettings settings)
+        {
+            var client = BuildClient(settings);
+
+            request.Method = Method.POST;
+            request.AddHeader("X-MediaBrowser-Token", settings.ApiKey);
+
+            var response = client.ExecuteAndValidate(request);
+            _logger.Trace("Response: {0}", response.Content);
+
+            CheckForError(response);
+
+            return response.Content;
+        }
+
+        private IRestClient BuildClient(MediaBrowserSettings settings)
+        {
+            var url = String.Format(@"http://{0}/mediabrowser", settings.Address);
+            
+            return RestClientFactory.BuildClient(url);
+        }
+
+        private void CheckForError(IRestResponse response)
+        {
+            _logger.Debug("Looking for error in response: {0}", response);
+
+            //TODO: actually check for the error
+        }
+    }
+}
